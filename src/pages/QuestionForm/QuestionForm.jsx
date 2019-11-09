6d62fe9f2e5a3d0309fd8f42742a160058255617
Steven Gibson <devgibsonsp@gmail.com>
2019-11-08 20:29:51 -0500

    removed step ui

--- src/pages/QuestionForm/QuestionForm.jsx
+++ src/pages/QuestionForm/QuestionForm.jsx
@@ -10,39 +10,10 @@ import {
 } from 'semantic-ui-react';
 
 
-const StepExampleGroup = () => (
-  <Step.Group>
-    <Step>
-      <Icon name='truck' />
-      <Step.Content>
-        <Step.Title>Shipping</Step.Title>
-        <Step.Description>Choose your shipping options</Step.Description>
-      </Step.Content>
-    </Step>
-    <Step active>
-      <Icon name='payment' />
-      <Step.Content>
-        <Step.Title>Billing</Step.Title>
-        <Step.Description>Enter billing information</Step.Description>
-      </Step.Content>
-    </Step>
-    <Step disabled>
-      <Icon name='info' />
-      <Step.Content>
-        <Step.Title>Confirm Order</Step.Title>
-      </Step.Content>
-    </Step>
-  </Step.Group>
-)
 
 const QuestionForm = (props) => {
   return (
     <React.Fragment>
-      <Grid.Column width={12}>
-        <Segment>
-          {StepExampleGroup()}
-        </Segment>
-      </Grid.Column>
       <Grid.Column>
         <Segment>
           Test 3
